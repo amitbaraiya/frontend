b55ce108a568d493a65166d3ea8e00bdb0fb45e7
amitbaraiya <amitbaraiya82@gmail.com>
2021-07-05 15:17:09 +0530

    update

--- config.js
+++ config.js
@@ -2,4 +2,5 @@ data_table = [
 	{dir:"html_tutorial", topic:"HTML Tutorial", skill:"HTML"},
 	{dir:"html_forms",topic:"HTML Forms",skill:"HTML"},
 	{dir:"html_graphics",topic:"HTML Graphics",skill:"HTML"},
+	{dir:"html_api",topic:"HTML API", skill:"HTML"},
 ]
